d188d16ae3ee5564caab1c071ff63f4aeba65a1d
legout <ligno.blades@gmail.com>
2023-06-07 16:31:25 +0200

    fixed typo

--- src/yfin/utils/proxy.py
+++ src/yfin/utils/proxy.py
@@ -1,5 +1,5 @@
 from parallel_requests.utils import (
-    set_webshare_proxy_url,
-    get_webshare_proxy_list,
-    get_free_proxy_list,
+    set_webshare_proxies_list,
+    get_webshare_proxies_list,
+    get_free_proxies_list,
 )
