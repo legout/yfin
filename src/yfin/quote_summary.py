ef003b131ef8816b5af04a8468a0ea60cacc60ce
legout <ligno.blades@gmail.com>
2023-06-18 16:58:01 +0200

    quote_summary returns a pandas dataframe now

--- src/yfin/quote_summary.py
+++ src/yfin/quote_summary.py
@@ -675,16 +675,34 @@ class QuoteSummary:
 async def quote_summary_async(symbols:str|list[str], modules:str|list[str]|None=None, *args, **kwargs):
     qs = QuoteSummary(symbols=symbols, modules=modules)
     await qs.fetch(*args, **kwargs)
+    results = {}
     if "earnings" in qs._modules:
         qs._format_earning()
         qs._modules.remove("earnings")
+        results["earnings"] = pd.concat(qs.results["earnings"], names=["symbol"]).reset_index().drop("level_1", axis=1)
     elif "earnings_trend" in qs._modules:
         qs._format_earnings_trends()
-        qs._modules.remove("earnings_trends")
+        qs._modules.remove("earnings_trend")
+        results["earnings_trend"] = pd.concat(qs.results["earnings_trend"], names=["symbol"]).reset_index().drop("level_1", axis=1)
     for module in qs._modules:
         qs._format_results(module=module)
+        if isinstance(qs.results[module], pd.Series):
+            if "symbol" not in res.columns:
+                res = pd.concat(qs["summary_profile"], names=["symbol"]).unstack().reset_index()
+            else:
+                res = pd.concat(qs["summary_profile"]).unstack().reset_index(drop=True)
+        elif isinstance(qs.results[module], pd.DataFrame):
+            res = pd.concat(qs.results["earnings_trend"], names=["symbol"])
+            if "symbol" not in res.columns:
+                res = res.reset_index().drop("level_1", axis=1)
+            else:
+                res = res.reset_index(drop=True)
+        else:
+            res = qs.results[module]
+        results[module] = res
+
+    return results
         
-    return qs.results
 
 
 def quote_summary(symbols:str|list[str], modules:str|list[str]|None=None, *args, **kwargs):
