1698853afdebb546f922e914a072496de0c99322
legout <ligno.blades@gmail.com>
2023-12-21 17:50:36 +0100

    v0.2.6

--- src/yfin/base.py
+++ src/yfin/base.py
@@ -11,8 +11,12 @@ class Session(ParallelRequests):
         random_proxy: bool = False,
         random_user_agent: bool = True,
         proxies: list | str | None = None,
-        user_agents: list | str | None = None,):
-        
+        user_agents: list | str | None = None,
+        cookies: dict | None = None,
+        verbose: bool = True,
+        debug: bool = False,
+        warnings: bool = False,
+    ) -> None:
         super().__init__(
             concurrency=concurrency,
             max_retries=max_retries,
@@ -21,14 +25,17 @@ class Session(ParallelRequests):
             random_user_agent=random_user_agent,
             proxies=proxies,
             user_agents=user_agents,
+            cookies=cookies,
+            verbose=verbose,
+            debug=debug,
+            warnings=warnings,
         )
         self._set_cookie_and_crumb()
-        
+
     def _get_cookie_and_crumb(self):
         yf = YfData(self._session)
         return yf._get_cookie_and_crumb()
-    
+
     def _set_cookie_and_crumb(self):
         cookies, self.crumb, _ = self._get_cookie_and_crumb()
         self._session.cookies.set_cookie(cookies)
-        
